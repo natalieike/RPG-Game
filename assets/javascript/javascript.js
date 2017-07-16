8ecbb40c5571dd89d017be83bb3f0d8cbc1bc439
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-07-15 17:42:36 -0700

    Complete game setup - user selects character and opponent, all images move to proper locations

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	var ninjas;  //game object holding all of the characters
+	var startBtn = $("#startBtn");
+	var attackBtn = $("#attackBtn");
 
 	//object constructor for each character
 	function character(aP, hP, pic){
@@ -27,25 +29,32 @@ $(document).ready(function(){
 	//Initialize the ninjas object with each character and display character choices
 	var initializeGame = function(){
 		var setup = $("#setup"); //space where images will be displayed
-
 		//set up ninjas object with initialized characters
 		ninjas = {
 			steve: new character(5, 160, "assets/images/angryNinja.jpeg"),
 			tiffany: new character(20, 100, "assets/images/girlNinja2.png"),
 			kenny: new character(15, 120, "assets/images/happyNinja2.jpg"),
 			bobby: new character(10, 140, "assets/images/doubleSwordNinja.png"),
+			userChar: "",
+			opponent: ""
 		};
 		//build HTML for showing character options
 		setup.html("<p class='center'>Please select your character</p>");
 		for (var i in ninjas){
-			var newImg = $("<img>");
-			newImg.attr("src", ninjas[i].picture);
-			newImg.attr("class", "ninjaImage");
-			newImg.attr("data-char", i);
-			setup.append(newImg);
+			if(jQuery.type(ninjas[i]) === "object"){
+				var newImg = buildNinjaImg(ninjas[i].picture, i);
+				setup.append(newImg);
+			};
 		};
 	};
 
+	var buildNinjaImg = function(imageFilename, idName){
+		var newImg = $("<img>");
+		newImg.attr("src", imageFilename);
+		newImg.attr("class", "ninjaImage");
+		newImg.attr("data-char", idName);
+		return(newImg);
+	}
 
 	var attack = function(attacker, opponent){
 		opponent.getAttacked(user.counterAttackPower);
@@ -68,22 +77,76 @@ $(document).ready(function(){
 		}
 	};
 
-	//var winMatch = function(){
+	//Moves user's selected character to the fight section and sets up selection of opponents
+	var moveUserChar = function(userSelect){
+		var fightArea = $("#fight");
+		var waitingArea = $("#waiting");
+		var newImg = buildNinjaImg(ninjas[userSelect].picture, userSelect);
+		fightArea.append(newImg);
+		$("#setup").html("<p>Please select an opponent</p>");
+		rebuildWaitingArea();
+	};
 
-	//};
+	var rebuildWaitingArea = function(){
+		var waitingArea = $("#waiting");
+		waitingArea.empty();
+		for (var i in ninjas){
+			if((jQuery.type(ninjas[i]) === "object") 
+			   && (i != ninjas.userChar) 
+			   && (i != ninjas.opponent)
+			   && (ninjas[i].defeated == false)){
+					var newImg = buildNinjaImg(ninjas[i].picture, i);
+					waitingArea.append(newImg);
+			};
+		};
+	}
 
-	//var loseGame = function(){
+	var moveOpponentChar = function(userSelect){
+		var fightArea = $("#fight");
+		var newImg = buildNinjaImg(ninjas[userSelect].picture, userSelect);
+		newImg.attr("class", "opponentImage");
+		fightArea.append(newImg);
+		rebuildWaitingArea();
+	}
 
-	//};
+	var winMatch = function(){
+		attackBtn.hide();
+		moveLoser(ninjas[ninjas.opponent].picture);
+		ninjas.opponent = "";
+	};
+
+	var loseGame = function(){
+		attackBtn.hide();
+		startBtn.show();	
+	};
 
 	//Hide Attack button until ready to attack
-	$("#attackBtn").hide();
+	attackBtn.hide();
 
-	$("#startBtn").click(function(){
+	//Click Start Button to initialize game.  Hide button once character options are exposed.
+	startBtn.click(function(){
 		initializeGame();
-		$("#startBtn").hide();
+		startBtn.hide();
 	});
 
+	//Click image to get user or to get opponent
+	$("body").on("click", ".ninjaImage", function(){
+		var selection = $(this).attr("data-char");
+		var fightArea = $("#fight");
+		if (ninjas.userChar === ""){
+			ninjas.userChar = selection;
+			moveUserChar(selection);
+		}
+		else if (ninjas.opponent === ""){
+			ninjas.opponent = selection;
+			moveOpponentChar(selection);
+			attackBtn.show();
+		}
+		else{
+			return;
+		}
+	});
+	
 	
 
 });
